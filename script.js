32676c3f7ab6c00c0afdb6e4089e85f87709bc34
Mateo Herrera Tamayo <teoherrera07@gmail.com>
2023-09-03 17:34:13 -0500

    montando script

---
+++ script.js
@@ -0,0 +1,26 @@
+const textArea = document.querySelector('.text-area');
+const mensaje = document.querySelector('.mensaje');
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncrypt(){
+  const textoEncriptado = encrypt(textArea.value);
+  mensaje.value = textoEncriptado;
+
+}
+
+function encrypt(stringEncriptado) {
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    };
+    return stringEncriptado;
+}
